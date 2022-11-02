a175575c5af6920d114c3dae0406e56ce59e39b5
huynguyenhekate <huynguyen@hekategroup.com>
2022-11-02 13:48:48 +0700

    ui

--- src/modules/modal/CreateJobs.jsx
+++ src/modules/modal/CreateJobs.jsx
@@ -5,9 +5,35 @@ import { Dropdown } from 'primereact/dropdown';
 import { Calendar } from 'primereact/calendar';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
+import { AutoComplete } from 'primereact/autocomplete';
+import { InputNumber } from 'primereact/inputnumber';
+import { useForm, Controller } from 'react-hook-form';
+import { classNames } from 'primereact/utils';
 
 const CreateJobs = ({ isOpenCreateJob, setIsOpenCreateJob }) => {
-    const [cities, setCities] = React.useState(null);
+    const defaultValues = {
+        type_job: null,
+        nameCustomer: '',
+        date: null,
+        type_image: "",
+        count: "",
+        original_link: '',
+        type_file: '',
+        cost: '',
+        status: null,
+        content: '',
+        note: '',
+    }
+    const { control, formState: { errors }, handleSubmit, reset } = useForm({ defaultValues });
+ 
+    const [filteredNameCustomers, setFilteredNameCustomers] = React.useState(null);
+
+    const names =[
+        {"name": "Afghanistan", "code": "AF"},
+        {"name": "Åland Islands", "code": "AX"},
+        {"name": "Albania", "code": "AL"},
+        {"name": "Algeria", "code": "DZ"},
+    ]
 
     const type_files = [
         { name: 'JPG', code: 'JPG' },
@@ -15,11 +41,40 @@ const CreateJobs = ({ isOpenCreateJob, setIsOpenCreateJob }) => {
         { name: 'PSD', code: 'PSD' },
     ];
 
-    const status= [
+    const type_status= [
         { name: 'Đang yêu cầu', code: 'DONE' },
         { name: 'Tạm hoãn yêu cầu', code: 'PENDING' },
         { name: 'Ngưng yêu cầu', code: 'STOP' },
     ]
+
+    const type_jobs = [
+        { name: 'Tính phí', code: 'CHARGE' },
+        { name: 'Không tính phí', code: 'NO CHARGE' },
+        { name: 'Chỉnh sửa', code: 'EDIT' },
+    ]
+
+    const searchName = (event) => {
+        setTimeout(() => {
+            let _filteredNameCustomers;
+            if (!event.query.trim().length) {
+                _filteredNameCustomers = [...names];
+            }
+            else {
+                _filteredNameCustomers = names.filter((item) => {
+                    return item.name.toLowerCase().startsWith(event.query.toLowerCase());
+                });
+            }
+
+            setFilteredNameCustomers(_filteredNameCustomers);
+        }, 50);
+    }
+    
+    const onSubmit = (data) => {
+        if(Object.keys(errors).length === 0){
+            console.log(data)
+            reset();
+        }
+    };
 
   return (
     <Sidebar visible={isOpenCreateJob} position="right" onHide={() => setIsOpenCreateJob(false)} className="create__job">
@@ -27,85 +82,193 @@ const CreateJobs = ({ isOpenCreateJob, setIsOpenCreateJob }) => {
             <div className="creat__job--title">
                 <h2>Tạo công việc mới</h2>
             </div>
-            <div className=" grid">
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Mã khách hàng : </h5>
-                        <InputText id="customer_id " />
-                    </span>
+            <form className=" grid modal__creat--job" onSubmit={handleSubmit(onSubmit)}>
+                <div className="field col-12 md:col-12 grid">
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="autocomplete">Nhập tên khách hàng: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                             <Controller name="nameCustomer" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <AutoComplete 
+                                suggestions={filteredNameCustomers}
+                                completeMethod={searchName} field="name"
+                                aria-label="Countries" 
+                                id={field.name}
+                                value={field.value} onChange={(e) => field.onChange(e.value)}
+                                className={classNames({ 'p-invalid': fieldState.invalid })}
+                                dropdownAriaLabel="Select name" 
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="type_job">Loại công việc: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="type_job" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <Dropdown  
+                                options={type_jobs}  
+                                optionLabel="name"  
+                                value={field.value} onChange={(e) => field.onChange(e.value)}
+                                className={classNames({ 'p-invalid': fieldState.invalid },"create__job_type")}
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6 create__job--calendar">
+                        <span htmlFor="calendar">Chọn ngày:<span className="warning">*</span></span>
+                        <span className="p-float-label ">
+                            <Controller name="date" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <Calendar 
+                                id={field.name} className={classNames({ 'p-invalid': fieldState.invalid })}
+                                value={field.value} onChange={(e) => field.onChange(e.value)}
+                                dateFormat="dd/mm/yy" mask="99/99/9999"
+                                />
+                            )} />
+                        </span>
+                        <img src="/images/calendar.svg" alt="" className="calendar__image"/>
+                    </div>
+                    <div className="field col-12 md:col-3">
+                        <span htmlFor="type_image">Loại ảnh: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="type_image" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputText id={field.name} 
+                                {...field} 
+                                className={classNames({ 'p-invalid': fieldState.invalid })} 
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-3 ">
+                        <span htmlFor="withoutgrouping">Số lượng: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="count" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputNumber id="count " 
+                                inputId="withoutgrouping" 
+                                value={field.value} onChange={(e) => field.onChange(e.value)} 
+                                mode="decimal" 
+                                useGrouping={false} 
+                                className={classNames({ 'p-invalid': fieldState.invalid })}
+                                placeholder="sss"
+                                />
+                            )} />
+                        </span>
+                    </div> 
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="original__link">Link ảnh gốc: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="original_link" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputText 
+                                id={field.name} 
+                                {...field}
+                                className={classNames({ 'p-invalid': fieldState.invalid })}
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="original__link">Định dạng file: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="type_file" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <Dropdown  
+                                options={type_files}  
+                                optionLabel="name"  
+                                value={field.value} onChange={(e) => field.onChange(e.value)}
+                                className={classNames({ 'p-invalid': fieldState.invalid },"create__job_type")}
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="cost">Chi phí: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="cost" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputNumber id="count " 
+                                inputId="currency-vn" 
+                                value={field.value} onChange={(e) => field.onChange(e.value)} 
+                                mode="currency" 
+                                currency="VND" 
+                                locale="vi-VN"
+                                useGrouping={false} 
+                                className={classNames({ 'p-invalid': fieldState.invalid })}
+                                placeholder="Nhập tên"
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6">
+                        <span htmlFor="employees">Trạng thái: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="status" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <Dropdown  
+                                options={type_status}  
+                                optionLabel="name"  
+                                value={field.value} onChange={(e) => field.onChange(e.value)}
+                                className={classNames({ 'p-invalid': fieldState.invalid },"create__job_type")}
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-12">
+                        <span htmlFor="employees">Nội dung yêu cầu: <span className="warning">*</span></span>
+                        <span className="p-float-label">
+                            <Controller name="content" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputTextarea  
+                                autoResize 
+                                id={field.name}
+                                {...field}
+                                className={classNames({ 'p-invalid': fieldState.invalid },"create__job_area")}
+                                />
+                            )} />
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-12">
+                        <span className="p-float-label">
+                            <span >Lưu ý của khách hàng:<span className="warning">*</span></span>
+                            <Controller name="note" 
+                                control={control} 
+                                rules={{ required: true }} render={({ field, fieldState }) => (
+                                <InputTextarea  
+                                autoResize 
+                                id={field.name}
+                                {...field}
+                                className={classNames({ 'p-invalid': fieldState.invalid },"create__job_area")}
+                                />
+                            )} />
+                        </span>
+                    </div>
                 </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Loại công việc : </h5>
-                        <InputText id="type_job" />
-                    </span>
+                <div className="btn_modal field col-12 md:col-12 grid">
+                    <div className="field col-12 md:col-6">
+                        <span className="p-float-label">
+                            <Button label="Hủy bỏ" className="p-button-outlined cancel--btn" onClick={()=>setIsOpenCreateJob(false)}/>
+                        </span>
+                    </div>
+                    <div className="field col-12 md:col-6">
+                        <span className="p-float-label">
+                            <Button label="Tạo mới" className="p-button-outlined p-button-secondary confirm--btn" type="submit"/>
+                        </span>
+                    </div>
                 </div>
-                <div className="field col-12 md:col-6 create__job--calendar">
-                    <span className="p-float-label ">
-                        <h5>Ngày hạn chốt công việc : </h5>
-                        <Calendar id="calendar"  className="calendar_don"/>
-                    </span>
-                    <img src="/images/calendar.svg" alt="" className="calendar__image"/>
-                </div>
-                <div className="field col-12 md:col-3">
-                    <span className="p-float-label">
-                        <h5>Loại ảnh : </h5>
-                        <InputText id="type_image " />
-                    </span>
-                </div>
-                <div className="field col-12 md:col-3 ">
-                    <span className="p-float-label">
-                        <h5>Số lượng : </h5>
-                        <InputText id="original__link " />
-                    </span>
-                </div> 
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Link ảnh gốc : </h5>
-                        <InputText id="original__link " />
-                    </span>
-                </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Loại ảnh : </h5>
-                        <Dropdown value={cities} options={type_files} onChange={(e)=>setCities(e.value)} optionLabel="name"  className="create__job_type"/>
-                    </span>
-                </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Chi phí : </h5>
-                        <InputText id="employees " />
-                    </span>
-                </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <h5>Trạng thái : </h5>
-                        <Dropdown  options={status}  optionLabel="name"  className="create__job_type"/>
-                    </span>
-                </div>
-                <div className="field col-12 md:col-12">
-                    <span className="p-float-label">
-                        <h5>Nội dung yêu cầu :</h5>
-                        <InputTextarea  autoResize className="create__job_area"/>
-                    </span>
-                </div>
-                <div className="field col-12 md:col-12">
-                    <span className="p-float-label">
-                        <h5>Lưu ý của khách hàng :</h5>
-                        <InputTextarea  autoResize  className="create__job_area"/>
-                    </span>
-                </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <Button label="Primary" className="p-button-outlined cancel--btn" />
-                    </span>
-                </div>
-                <div className="field col-12 md:col-6">
-                    <span className="p-float-label">
-                        <Button label="Secondary" className="p-button-outlined p-button-secondary confirm--btn" />
-                    </span>
-                </div>
-            </div>
+            </form>
         </div>
     </Sidebar>
   )
