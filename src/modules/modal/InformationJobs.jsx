5eb8a818c83feb69d46f22490d9d0fbca0014e3b
quanglongdevDN <longthanh@hekategroup.com>
2022-11-18 01:34:02 +0700

    update ui create job

--- src/modules/modal/InformationJobs.jsx
+++ src/modules/modal/InformationJobs.jsx
@@ -9,14 +9,24 @@ import { setIsOpenInformationJob } from '../../redux/modal/modalSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from "react-hook-form";
 import { Dropdown } from 'primereact/dropdown';
-import { type_status_jobs, type_jobs } from "./dropDown";
-import { UserRules, JobRules } from "../../constants"
+import { type_status_jobs, type_jobs, type_files } from "./dropDown";
+import { UserRules, JobRules } from "../../constants";
+import { InputText } from 'primereact/inputtext';
+import { timezoneToDate } from '../../commons/dateTime';
+
+
 const InformationJobs = () => {
     const toast = useRef(null);
     const dispatch = useDispatch()
 
     const [editStatusJobs, setEditStatusJobs] = useState(false);
     const [editTypeJobs, setEditTypeJobs] = useState(false);
+    const [editQuality, setEditQuality] = useState(false);
+    const [editModels, setEditModels] = useState(false);
+    const [editFile, setEditTypeFile] = useState(false);
+    const [editOrgLink, setEditOrgLink] = useState(false);
+
+    const [typeFile, setTypeFile] = useState(false);
     const [statusJobs, setStatusJobs] = useState(false);
     const [typeJobs, setTypeJobs] = useState(false);
 
@@ -27,7 +37,6 @@ const InformationJobs = () => {
 
 
     useEffect(() => {
-        console.log(rowdata?.data);
         if (rowdata?.data?.status_jobs) {
             for (let item of type_status_jobs) {
                 if (item.code === rowdata?.data?.status_jobs) {
@@ -35,10 +44,19 @@ const InformationJobs = () => {
                     break
                 }
             }
-        } if (rowdata?.data?.work_types) {
+        }
+        if (rowdata?.data?.work_types) {
             for (let item of type_jobs) {
                 if (item.code === rowdata?.data?.work_types) {
                     setTypeJobs(item)
+                    break
+                }
+            }
+        }
+        if (rowdata?.data?.photo_types) {
+            for (let item of type_files) {
+                if (item.code === rowdata?.data?.photo_types) {
+                    setTypeFile(item)
                     break
                 }
             }
@@ -66,11 +84,13 @@ const InformationJobs = () => {
     };
 
     const handleCloseModal = () => {
-        dispatch(setIsOpenInformationJob(false))
-        setEditStatusJobs(false)
-        setEditTypeJobs(false)
-        // setEditRole(false)
-        // setEditUsername(false)
+        dispatch(setIsOpenInformationJob(false));
+        setEditStatusJobs(false);
+        setEditTypeJobs(false);
+        setEditModels(false);
+        setEditQuality(false);
+        setEditTypeFile(false);
+        setEditOrgLink(false);
         reset()
     }
 
@@ -91,13 +111,56 @@ const InformationJobs = () => {
                         <div className="field col-12 md:col-12 grid">
                             <div className="field col-12 md:col-6">
                                 <span htmlFor="autocomplete">Mã công việc :</span>
-                                <span className="p-float-label pt-3 flex justify-content-between cursor__normal">
-                                    <span className='font-bold'>{rowdata?.data?.id_system}</span>
+                                <span className="p-float-label mt-3 flex justify-content-between cursor__normal">
+                                    <span className='font-bold mt-1'>{rowdata?.data?.id_system}</span>
                                     <img src="images/copy.svg" alt="" label="Bottom Right" onClick={copyToClipboard} className="cursor-pointer" />
                                 </span>
                             </div>
+                            <div className="field col-12 md:col-6 ">
+                                <span htmlFor="typejobs">Loại ảnh :<span className="warning">*</span></span>
+                                <span className="p-float-label  flex justify-content-between align-items-center cursor__edit">
+                                    <span onClick={(e) => setEditQuality(true)} className="col-12 md:col-3">
+                                        {editQuality ?
+                                            (
+                                                <InputText
+                                                    onKeyPress={(event) => {
+                                                        if (!/[0-9]/.test(event.key)) {
+                                                            event.preventDefault();
+                                                        }
+                                                    }}
+                                                    defaultValue={rowdata?.data?.quality}
+                                                    onChange={(e) => setValue("quality", e.target.value)}
+                                                    {...register("quality", { required: true, min: 1 })}
+                                                    className={errors?.quality && "p-invalid"}
+                                                />
+                                            ) : (
+                                                <span className='p-float-label mt-3'>
+                                                    <span className='font-bold'>{rowdata?.data?.quality}</span>
+                                                </span>
+                                            )
+                                        }
+                                    </span>
+                                    <span className='mt-3'> - </span>
+                                    <span onClick={(e) => setEditModels(true)} className="col-12 md:col-9">
+                                        {editModels ?
+                                            (
+                                                <InputText
+                                                    defaultValue={rowdata?.data?.type_models}
+                                                    onChange={(e) => setValue("type_models", e.target.value)}
+                                                    {...register("type_models", { required: true, })}
+                                                    className={errors?.type_models && "p-invalid"}
+                                                />
+                                            ) : (
+                                                <span className='p-float-label mt-3'>
+                                                    <span className='mt-3 font-bold'>{rowdata?.data?.type_models}</span>
+                                                </span>
+                                            )
+                                        }
+                                    </span>
+                                </span>
+                            </div>
                             <div className="field col-12 md:col-6">
-                                <span htmlFor="statusjobs">Trạng thái công việc ( SALER ):</span>
+                                <span htmlFor="statusjobs">Trạng thái công việc ( SALER ):<span className="warning">*</span></span>
                                 <span onClick={(e) => setEditStatusJobs(true)} className="p-float-label cursor__edit">
                                     {editStatusJobs ?
                                         (
@@ -118,7 +181,7 @@ const InformationJobs = () => {
                                 </span>
                             </div>
                             <div className="field col-12 md:col-6">
-                                <span htmlFor="typejobs">Loại công việc :</span>
+                                <span htmlFor="typejobs">Loại công việc :<span className="warning">*</span></span>
                                 <span onClick={(e) => setEditTypeJobs(true)} className="p-float-label cursor__edit">
                                     {editTypeJobs ?
                                         (
@@ -138,41 +201,98 @@ const InformationJobs = () => {
                                     }
                                 </span>
                             </div>
-                            <div className="field col-12 md:col-6">
-                                <span htmlFor="typejobs">Loại ảnh :</span>
-                                <span className="p-float-label pt-3 flex align-items-center cursor__normal">
-                                    <span className="p-float-label cursor__normal">
-                                        <span className='font-bold'>{rowdata?.data?.quality}</span>
-                                    </span>
-                                    <span>&emsp; - &emsp;</span>
-                                    <span className="p-float-label cursor__normal">
-                                        <span className='font-bold'>{rowdata?.data?.type_models}</span>
-                                    </span>
+                            <div className="field col-12 md:col-6 create__job--calendar">
+                                <span htmlFor="start_day">Ngày tạo công việc:</span>
+                                <span className="p-float-label pt-3 cursor__normal font-bold">
+                                    {timezoneToDate(rowdata?.data.start_day)}
                                 </span>
+                            </div>
+                            <div className="field col-12 md:col-6 create__job--calendar">
+                                <span htmlFor="end_day">Ngày hạn chót công việc: <span className="warning">*</span></span>
+                                <span className="p-float-label pt-3 cursor__normal font-bold">
+                                    {timezoneToDate(rowdata?.data.end_day)}
+                                </span>
+                            </div>
+                            <div className="field col-12 md:col-6">
+                                <span htmlFor="statusjobs">Định dạng file:<span className="warning">*</span></span>
+                                <span onClick={(e) => setEditTypeFile(true)} className="p-float-label cursor__edit">
+                                    {editFile ?
+                                        (
+                                            <Dropdown
+                                                options={type_files}
+                                                optionLabel="name"
+                                                defaultValue={typeFile}
+                                                value={typeFile}
+                                                onChange={e => setTypeFile(e.value)}
+                                                disabled={(user?.data?.role === UserRules.ROLE.EDITOR && user?.data?.role === UserRules.ROLE.LEADER_EDITOR) ? true : false}
+                                            />
+                                        ) : (
+                                            <span className="p-float-label mt-3 flex justify-content-between align-items-center font-bold" >
+                                                {JobRules.PHOTO_TYPES[rowdata?.data?.photo_types]}
+                                            </span>
+                                        )
+                                    }
+                                </span>
+                            </div>
+                            <div className="field col-12 md:col-6">
+                                <span htmlFor="statusjobs">Editor:<span className="warning">*</span></span>
+                                <span onClick={(e) => setEditTypeFile(true)} className="p-float-label cursor__edit">
+                                    {editFile ?
+                                        (
+                                            <Dropdown
+                                                options={type_files}
+                                                optionLabel="name"
+                                                defaultValue={typeFile}
+                                                value={typeFile}
+                                                onChange={e => setTypeFile(e.value)}
+                                                disabled={(user?.data?.role === UserRules.ROLE.EDITOR && user?.data?.role === UserRules.ROLE.LEADER_EDITOR) ? true : false}
+                                            />
+                                        ) : (
+                                            <span className="p-float-label mt-3 flex justify-content-between align-items-center font-bold" >
+                                                {JobRules.PHOTO_TYPES[rowdata?.data?.photo_types]}
+                                            </span>
+                                        )
+                                    }
+                                </span>
+                            </div>
+                            <div className="field col-12 md:col-6 create__job--calendar">
+                                <span htmlFor="start_day">Link ảnh gốc:<span className="warning">*</span></span>
+                                <span onClick={(e) => setEditOrgLink(true)} className="p-float-label mt-3 cursor__edit">
+                                    {editOrgLink ?
+                                        (
+                                            <InputText
+                                                defaultValue={rowdata?.data?.org_link}
+                                                onChange={(e) => setValue("org_link", e.target.value)}
+                                                {...register("org_link", { required: true, min: 1 })}
+                                                className={errors?.quality && "p-invalid"}
+                                            />
+                                        ) : (
+                                            <span className='font-bold cursor__normal'>
+                                                <a href={rowdata?.data.org_link} target="_blank" rel="noreferrer">Link liên kết</a>
+                                            </span>
+                                        )
+                                    }
 
-                            </div>
-                            <div className="field col-12 md:col-6">
-                                <span className="p-float-label flex justify-content-between">
-                                    <strong className="information_job_title">Số lượng ảnh :</strong>
-                                    <span>99</span>
                                 </span>
                             </div>
-                            <div className="field col-12 md:col-6">
-                                <span className="p-float-label flex justify-content-between">
-                                    <strong className="information_job_title ">Link ảnh gốc :</strong>
-                                    <a href="https://www.freecodecamp.org/" target="_blank" rel="noreferrer">link liên kết</a>
-                                </span>
-                            </div>
-                            <div className="field col-12 md:col-6">
-                                <span className="p-float-label flex justify-content-between">
-                                    <strong className="information_job_title">Định dạng file :</strong>
-                                    <span>PSD</span>
-                                </span>
-                            </div>
-                            <div className="field col-12 md:col-6 ">
-                                <span className="p-float-label flex justify-content-between">
-                                    <strong className="information_job_title">Số tiền nhận được :</strong>
-                                    <span>100.000đ</span>
+                            <div className="field col-12 md:col-6 create__job--calendar">
+                                <span htmlFor="start_day">Link ảnh gốc:<span className="warning">*</span></span>
+                                <span onClick={(e) => setEditOrgLink(true)} className="p-float-label mt-3 cursor__edit">
+                                    {editOrgLink ?
+                                        (
+                                            <InputText
+                                                defaultValue={rowdata?.data?.org_link}
+                                                onChange={(e) => setValue("org_link", e.target.value)}
+                                                {...register("org_link", { required: true, min: 1 })}
+                                                className={errors?.quality && "p-invalid"}
+                                            />
+                                        ) : (
+                                            <span className='font-bold cursor__normal'>
+                                                <a href={rowdata?.data.org_link} target="_blank" rel="noreferrer">Link liên kết</a>
+                                            </span>
+                                        )
+                                    }
+
                                 </span>
                             </div>
                             <div className="field col-12 md:col-12">
