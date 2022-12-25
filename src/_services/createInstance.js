88ee36c81657f9029cf4e8a0e16e1add927519c7
huynguyenhekate <huynguyen@hekategroup.com>
2022-12-25 22:54:30 +0700

    update token

--- src/_services/createInstance.js
+++ src/_services/createInstance.js
@@ -8,11 +8,12 @@ const URL = process.env.REACT_APP_API || process.env.REACT_APP_DEV_API
 
 export const createAxios = () =>{
   const token = storage.get(NAME_SESSION_STORAGE_TOKEN)
+
   const logout = ()=>{
     storage.delete(NAME_SESSION_STORAGE_TOKEN)
     setTimeout(() =>{
       window.location.href = "/login"
-    },300)
+    },700)
   } 
     const newInstance = axios.create()
     newInstance.interceptors.request.use(
@@ -28,12 +29,11 @@ export const createAxios = () =>{
             }
             let date = new Date()
             let default_token = token;
-            if(decodedToken.exp * 1000 - (60 * 1000 * 5) <= date.getTime()){
+            if(decodedToken.exp * 1000 <= date.getTime()){
               const formData = {
                 _id_activity : decodedToken._id
               }
               const refreshtoken = await refreshToken(formData)
-
               if(refreshtoken?.data?.access_token){
                 default_token = refreshtoken?.data?.access_token
                 storage.save(NAME_SESSION_STORAGE_TOKEN,default_token)
