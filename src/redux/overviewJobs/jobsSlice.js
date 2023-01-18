d2c374f8d3d422df23d64b2599ab3d5c13f583e2
huynguyenhekate <huynguyen@hekategroup.com>
2023-01-18 12:08:18 +0700

    update3

--- src/redux/overviewJobs/jobsSlice.js
+++ src/redux/overviewJobs/jobsSlice.js
@@ -67,11 +67,7 @@ const jobsReducer = createSlice({
                     loading: false,
                     data: null,
                     error: false,
-<<<<<<< src/redux/overviewJobs/jobsSlice.js
                 }
-=======
-                },
->>>>>>> src/redux/overviewJobs/jobsSlice.js
             })
         },
         resetJobCreated:(state)=>{
